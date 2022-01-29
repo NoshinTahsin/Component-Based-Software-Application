259dde61e5a1bd9b0a1465ecabebe59ae7ea0e28
NoshinTahsin <35559870+NoshinTahsin@users.noreply.github.com>
2022-01-29 17:17:26 +0600

    class library simple calculator
    performs some basic operations (add, sub, multiply, divide, and, or)

---
+++ ClassLibrarySimpleCalculator.cs
@@ -0,0 +1,57 @@
+﻿using System;
+
+namespace MyCalculator
+{
+    interface ICalculator
+    {
+        public double add(double num1, double num2);
+        public double subtract(double num1, double num2);
+        public double multiply(double num1, double num2);
+        public double divide(double num1, double num2);
+
+    }
+
+    interface ILogicalCalculator
+    {
+        public bool and(bool var1, bool var2);
+        public bool or(bool var1, bool var2);
+
+    }
+
+    public class Calculator:ICalculator, ILogicalCalculator
+    {
+        //function to perform addition 
+        public double add(double num1, double num2)
+        {
+            return num1 + num2;
+        }
+
+        //function to perform subtraction 
+        public double subtract(double num1, double num2)
+        {
+            return num1 - num2;
+        }
+
+        //function to perform multiplication 
+        public double multiply(double num1, double num2)
+        {
+            return num1 * num2;
+        }
+        
+        //function to perform division
+        public double divide(double num1, double num2)
+        {
+                return num1 / num2;
+        }
+
+        public bool and(bool var1, bool var2)
+        {
+            return var1 && var2;
+        }
+
+        public bool or(bool var1, bool var2)
+        {
+            return var1 || var2;
+        }
+    }
+}
